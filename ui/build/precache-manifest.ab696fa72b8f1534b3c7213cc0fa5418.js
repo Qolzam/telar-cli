4d933726942b9db2dae59ebff26d1b0181c00acc
Amir Movahedi <amir.gholzam@live.com>
2020-03-21 21:03:37 +0700

    Add build.

---
+++ ui/build/precache-manifest.ab696fa72b8f1534b3c7213cc0fa5418.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7b0991b6516d0629df26210d29c777",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02db322fbf95ba6f226a",
+    "url": "/static/css/main.ab2c6aa1.chunk.css"
+  },
+  {
+    "revision": "2b55e221abd5785b3726",
+    "url": "/static/js/2.192763cd.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.192763cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02db322fbf95ba6f226a",
+    "url": "/static/js/main.3b06cd47.chunk.js"
+  },
+  {
+    "revision": "47bf3395871493a86dac",
+    "url": "/static/js/runtime-main.c8bea52d.js"
+  }
+]);
